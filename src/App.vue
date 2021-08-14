673a889e15ae7372d591b086b40b07aaf70a5e56
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-08-13 20:54:01 -0400

    formulario com vuejs e boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Formulario/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formulario from './components/Formulario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Formulario
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-image: url('assets/bg-intro-mobile.png');
+    background-color: #ff7978;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,77 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row h-100">
+                <div class="col-6">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-12">
+                            <h1 class="text-white">Learn to code by watching others</h1>
+                            <p class="text-white">
+                                See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+                                but understanding how developers think is invaluable.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-12 p-0">
+                            <a href="" class="oferta"><strong>Try it free 7 days</strong> then $20/mo. thereafter</a>
+                        </div>
+                        <div class="col-12 p-0 mt-5">
+                            <form action="" class="bg-white p-5">
+                                <input type="text" class="form-control p-4 mb-4" id="FirstName" placeholder="First Name">
+                                <input type="text" class="form-control p-4 mb-4" id="LastName" placeholder="Last Name">
+                                <input type="email" class="form-control p-4 mb-4" id="FirstName" placeholder="Email Address">
+                                <input type="password" class="form-control p-4 mb-4" placeholder="Password">
+                                <a href="" class="button-submit"><strong>CLAIM YOUR FREE TRIAL</strong></a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+    .container{
+        height: 650px;
+        margin-top: 100px;
+    }
+
+    .container h1{
+        font-size: 70px;
+    }
+
+    .container p{
+        font-size: 21px;
+    }
+
+    form{
+        border-radius: 5px;
+        height: 535px;
+    }
+
+    a{
+        display: block;
+        text-decoration: none;
+        color: #FFF;
+        background: #5d54a3;
+        border: 1px solid #6a5195;
+        padding: 20px 0;
+        text-align: center;
+        border-radius: 5px;
+    }
+
+    .button-submit{
+        background: #77e2b4;
+        border: 1px solid #93d3b9;
+    }
+    
+</style>
